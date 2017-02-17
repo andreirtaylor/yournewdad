92ac20a121337ea253c590049a07218adbe62a41
Andrei Taylor <andreirtaylor@gmail.com>
2017-02-16 23:43:56 -0800

    Update basic snake to 2017 rules

--- main.go
+++ main.go
@@ -1,23 +1,10 @@
-package main
+package kaa
 
 import (
-	"log"
 	"net/http"
-	"os"
 )
 
-func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
-
+func init() {
 	http.HandleFunc("/start", handleStart)
 	http.HandleFunc("/move", handleMove)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "9000"
-	}
-
-	log.Printf("Running server on port %s...\n", port)
-	http.ListenAndServe(":"+port, nil)
 }
